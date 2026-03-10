4b3a1d30d42173d8ac10547e4fa24bddc3175109
markestom0512-max <markestom0512@gmail.com>
2026-03-10 20:54:22 +0100

    Update config.js

--- web/config.js
+++ web/config.js
@@ -4,5 +4,5 @@
 //  Supabase : Settings > API dans votre dashboard
 // ============================================================
 
-window.SUPABASE_URL      = 'https://VOTRE_PROJECT_ID.supabase.co';
-window.SUPABASE_ANON_KEY = 'VOTRE_ANON_KEY';
+window.SUPABASE_URL      = https://eravmidvmoeugplookiy.supabase.co;
+window.SUPABASE_ANON_KEY = sb_publishable_mppZhkyvSKBqCnmsLIo2EA_vV4eA07A;
